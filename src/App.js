19ae48e76f3cb58d9fad6e74e98bc7e4faecbf45
ARandomPotatoPerson <richardzhao2002@gmail.com>
2023-01-13 08:53:30 -0500

    Yes
    I'm too tired for this rn.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import axios from "axios";
+import {useInterval} from "usehooks-ts";
+
 
 function RemovePerson(props) {
     return (
@@ -21,35 +25,86 @@ const initList = [];
 
 function App() {
 
+    let loading = true;
+
     const [list, setList] = React.useState(initList);
     const [name, setName] = React.useState('');
+
+    const refresh = async() => {
+        const response = await axios.get('https://mockend.com/ARandomPotatoPerson/Queue-for-CS2050/posts')
+        if (Array.isArray(response.data)) {
+            setList(response.data);
+            loading = false;
+        }
+    }
+
+    useEffect(() => {
+        refresh();
+    })
+
+    useInterval(() => {
+        refresh();
+        }, 1000
+    )
+
+
+    function clickRemove() {
+        /*
+        const tempList = [...list];
+        tempList.shift();
+        setList(tempList);
+        */
+
+    }
+
+    function clickAdd(name) {
+        axios.put('https://mockend.com/ARandomPotatoPerson/Queue-for-CS2050/posts/' + list.length, {
+            name: name
+        })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error.response.data)
+            });
+        refresh();
+    }
 
     function textChange(event) {
         setName(event.target.value);
     }
 
-    function clickRemove() {
-        const tempList = [...list];
-        tempList.shift();
-        setList(tempList);
-    }
-
-    function clickAdd(name) {
-        const tempList = list.concat({name: name});
-        setList(tempList);
-    }
-
     return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
         <div className="App">
-            <RemovePerson onClick={() => clickRemove()}/>
+
             <AddPerson onClick={() => clickAdd(name)}/>
+            <br></br>
             <input type="text" value={name} onChange={textChange}></input>
-            <ul>
-                {list.map((item) => (
-                    <li>{item.name}</li>
-                ))}
-            </ul>
         </div>
+                } />
+                <Route path="testingError" element={<hi>Fuck Off </hi>}/>
+                <Route path="TASite" element = {loading ?
+                    <div className="App">
+
+                        <RemovePerson onClick={() => clickRemove()}/>
+                        <br></br>
+                        <AddPerson onClick={() => clickAdd(name)}/>
+                        <br></br>
+
+                        <input type="text" value={name} onChange={textChange}></input>
+                        <ul>
+                            {list.map((item) => (
+                                <li>{item.name}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    : <div>Loading</div>
+                } />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//<Route path="/" element={<App initList={initList} value={initList.length}/>} />
+//<Route path="/Remove" element={<App initList={initList} value={initList.length}/>} />
+
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
